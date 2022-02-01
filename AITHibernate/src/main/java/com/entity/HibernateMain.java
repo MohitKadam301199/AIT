7916a2be370b22c5e0019a1fbafc2d823f43e649
Mohit Kadam <Mohit Kadam@DESKTOP-374HTF9>
2022-02-01 18:13:33 +0530

    2nd commit

--- AITHibernate/src/main/java/com/entity/HibernateMain.java
+++ AITHibernate/src/main/java/com/entity/HibernateMain.java
@@ -7,22 +7,26 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateMain {
 
+	
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//		Configuration cfg=new Configuration();
+//		cfg.configure();
+//		SessionFactory sessionfactory=cfg.buildSessionFactory();
+//		Session session=sessionfactory.openSession();
+//		Transaction tx=session.beginTransaction();
+//		Student s1=new Student();
+//		s1.setSid(5);
+//		s1.setSname("Rushi");
+//		s1.setPercent(95);
+//		session.save(s1);
+//		tx.commit();
+//		
+//
+//	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Configuration cfg=new Configuration();
-		cfg.configure();
-		SessionFactory sessionfactory=cfg.buildSessionFactory();
-		Session session=sessionfactory.openSession();
-		Transaction tx=session.beginTransaction();
-		Student s1=new Student();
-		s1.setSid(2);
-		s1.setSname("Mohit");
-		s1.setPercent(95);
-		session.save(s1);
-		tx.commit();
-		
-
+		System.out.println("hiiiiiiiii");
 	}
-
 }
